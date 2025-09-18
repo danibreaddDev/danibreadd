e97382584a453ccfe24f01fb68132c2c3be75442
Dani <danielpamblancomestre@gmail.com>
2025-09-18 17:39:25 +0200

    cambios

---
+++ src/lib/interfaces/experience.ts
@@ -0,0 +1,9 @@
+export interface iExperience {
+  slug: string;
+  position: string;
+  company: string;
+  descriptionJob: string;
+  technologies: string[];
+  startDate: string;
+  duration: string;
+}
